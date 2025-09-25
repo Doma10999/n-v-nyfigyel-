822a4bf4a473d01672f3b1a4fdef4fb477ce8c5f
Doma10999 <152029437+Doma10999@users.noreply.github.com>
2025-09-25 20:46:30 +0200

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,12 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+
+
+OneSignal.push(function() {
+  OneSignal.sendSelfNotification(
+    "Növényfigyelő",
+    "A növény szomjas! Locsold meg! 🌱💧",
+    'https://novenyfigyelo.netlify.app', // opcionális URL értesítés kattintásra
+    'https://example.com/notification-icon.png', // opcionális ikon
+    { notificationType: 'moistureAlert' }
+  );
+});
