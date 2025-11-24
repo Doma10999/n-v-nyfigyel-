69e035c82660d6546149491975d212e3aae15aa5
Doma10999 <152029437+Doma10999@users.noreply.github.com>
2025-11-24 20:12:43 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {};
+  self.registration.showNotification(data.title || 'Növényfigyelő', {
+    body: data.body || 'A növény vízszintje kritikusan alacsony!',
+    icon: data.icon || '/icon.png'
+  });
+});
