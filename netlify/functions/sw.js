6cee90b2c0fae0a93d7ad0c6ffe2eae28947e28a
Doma10999 <152029437+Doma10999@users.noreply.github.com>
2025-12-02 18:44:53 +0100

    Delete netlify/functions/sw.js

--- netlify/functions/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : {};
-  self.registration.showNotification(data.title || 'Növényfigyelő', {
-    body: data.body || 'A növény vízszintje kritikusan alacsony!',
-    icon: data.icon || '/icon.png'
-  });
-});
