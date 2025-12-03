10cb72daebb23432f88e3d714936cee2d7e0df9b
Doma10999 <152029437+Doma10999@users.noreply.github.com>
2025-12-03 20:07:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-self.addEventListener('push', function(event) {
+// Service Worker – Push kezelés
+
+self.addEventListener("push", function (event) {
   const data = event.data ? event.data.json() : {};
-  self.registration.showNotification(data.title || 'Növényfigyelő', {
-    body: data.body || 'A növény vízszintje kritikusan alacsony!',
-    icon: data.icon || '/icon.png'
+
+  self.registration.showNotification(data.title || "Növényfigyelő", {
+    body: data.body || "A növény vízszintje kritikusan alacsony!",
+    icon: data.icon || "/icon.png",
+    badge: "/icon.png",
   });
 });
