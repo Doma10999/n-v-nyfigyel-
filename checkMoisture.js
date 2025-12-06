4faff66321fa5f61ca777cf75f36ceff39ffd904
Doma10999 <152029437+Doma10999@users.noreply.github.com>
2025-12-06 17:50:04 +0100

    Delete checkMoisture.js

--- checkMoisture.js
+++
@@ -1,6 +0,0 @@
-// Egyszerű wrapper: ugyanazt csinálja, mint a sendPush
-const handlerSend = require("./sendPush").handler;
-
-exports.handler = async (event, context) => {
-  return handlerSend(event, context);
-};
