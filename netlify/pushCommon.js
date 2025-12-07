c08a03d8e39d68a9d15f36e835e1aeeca0aed004
Doma10999 <152029437+Doma10999@users.noreply.github.com>
2025-12-07 15:50:24 +0100

    Add files via upload

--- netlify/pushCommon.js
+++ netlify/pushCommon.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 const webpush = require("web-push");
-const serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 let app;
 let dbInstance = null;
